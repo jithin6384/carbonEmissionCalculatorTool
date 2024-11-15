634585761f1de8510b108e31815974516ecaa61b
Jithin R <raghunathan.jithin@gmail.com>
2024-11-16 00:00:38 +0100

    created basic user model

--- app.py
+++ app.py
@@ -1,10 +1,115 @@
 from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask import render_template, redirect, request, url_for, flash,abort
+from flask_login import login_user,login_required,logout_user
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+from forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+login_manager = LoginManager()
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+login_manager.init_app(app)
+
+login_manager.login_view = "login"
+
+# defining users here 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+
+    
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        
+        return check_password_hash(self.password_hash,password)
+
+
 @app.route('/')
 def home():
-    return "Welcome to the Carbon Footprint Monitoring Tool!"
+    return render_template('home.html')
+
+
+@app.route('/welcome')
+@login_required
+def welcome_user():
+    return render_template('welcome_user.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You logged out!')
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        
+        user = User.query.filter_by(email=form.email.data).first()
+       
+
+        if user.check_password(form.password.data) and user is not None:
+        
+
+            login_user(user)
+            flash('Logged in successfully.')
+
+      
+            next = request.args.get('next')
+
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering! Now you can login!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
